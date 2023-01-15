2da810714bf86cd56777c069fccb0325bbd878e3
ali aghakhani <icic@alis-MacBook-Air.local>
2023-01-15 12:46:32 +0330

    change function name

--- pages/index.vue
+++ pages/index.vue
@@ -46,7 +46,7 @@ export default {
     };
   },
   methods: {
-    async getkeys() {
+    async getKeyValues() {
       for (const [key, value] of Object.entries(this.posts.result)) {
         // console.log(`key: ${key}: value : ${value.total_activity} , ${value.total_task}`);
         this.options.xaxis.categories.push(key);
@@ -71,7 +71,7 @@ export default {
     this.posts = result;
   },
   async mounted() {
-    await this.getkeys();
+    await this.getKeyValues();
   },
 };
 </script>
